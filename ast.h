9ce364600bc91e34f896f9641d7614cd0ccf24f5
newenclave <kettunen.villu@gmail.com>
2017-06-14 02:03:15 +0300

    ast, parsetr

--- ast.h
+++ ast.h
@@ -5,8 +5,33 @@
 
 namespace mico { namespace ast {
 
-    struct node {
+    enum class node_type {
+        NONE = 0,
+        STATE,
+        EXPR,
+    };
 
+    struct node {
+        virtual ~node( ) { }
+        virtual node_type type( ) const
+        {
+            return node_type::NONE;
+        }
+        virtual std::string literal( ) const = 0;
+    };
+
+    struct statement: public node {
+        virtual node_type type( ) const
+        {
+            return node_type::STATE;
+        }
+    };
+
+    struct expression: public node {
+        virtual node_type type( ) const
+        {
+            return node_type::EXPR;
+        }
     };
 
 }}
